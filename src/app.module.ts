2d8153e6cbef61b0f1bd04dcfddf7bc0ebceb773
Timothy-py <adeyeyetimothy33@gmail.com>
2025-11-05 11:32:33 +0100

    feat:<Common>:setup common modules and configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,34 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Logger, Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import envConfiguration from './common/env.configuration';
+import { envValidation } from './common/env.validation';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSourceOptions } from 'typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      load: [envConfiguration],
+      isGlobal: true,
+      cache: true,
+      validate: envValidation,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService): DataSourceOptions => {
+        return {
+          type: 'sqlite',
+          database: ':memory',
+          synchronize: true, // TODO: Change to false in Production
+          logging: ['error', 'warn'],
+          entities: [],
+          // migrations: [],
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [],
+  providers: [Logger],
 })
 export class AppModule {}
