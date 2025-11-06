6f7c672a090d1b86f60e4febe908963989d43733
Timothy-py <adeyeyetimothy33@gmail.com>
2025-11-06 23:40:49 +0100

    fx

--- src/resources/offers/offers.service.ts
+++ src/resources/offers/offers.service.ts
@@ -26,6 +26,7 @@ export class OffersService {
   constructor(
     @InjectRepository(Offer)
     private readonly offerRepository: Repository<Offer>,
+    @InjectRepository(Vehicle)
     private readonly vehicleRepository: Repository<Vehicle>,
     private readonly loanService: LoansService,
   ) {}
